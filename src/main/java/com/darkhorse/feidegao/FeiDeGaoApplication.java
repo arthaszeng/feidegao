52668d9ebb15491045c57d1247973529748e25d6
Chaowei Zeng <sli616@163.com>
2022-03-12 21:21:50 +0800

    chore: initialize project.

---
+++ src/main/java/com/darkhorse/feidegao/FeiDeGaoApplication.java
@@ -0,0 +1,13 @@
+package com.darkhorse.feidegao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeiDeGaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeiDeGaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darkhorse/feidegao/FeiDeGaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkhorse.feidegao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeiDeGaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
